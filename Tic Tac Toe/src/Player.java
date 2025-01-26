19bba234d189e98e43b9af4b28295ab4476a00ca
Maher1506 <68017554+Maher1506@users.noreply.github.com>
2025-01-26 18:32:18 +0200

    created the grid and player classes

---
+++ Tic Tac Toe/src/Player.java
@@ -0,0 +1,21 @@
+public class Player {
+
+    private int score;
+    private String name;
+
+    //getters
+    public int getScore() {
+        return score;
+    }
+    public String getName() {
+        return name;
+    }
+
+    // setters
+    public void setScore(int newScore) {
+        score = newScore;
+    } 
+    public void setName(String newName) {
+        name = newName;
+    }
+}
