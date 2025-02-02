3ec852894470dde2777b1611d2ea494f1a603163
Maher1506 <68017554+Maher1506@users.noreply.github.com>
2025-02-02 16:39:03 +0200

    added replayability

--- Tic Tac Toe/src/Main.java
+++ Tic Tac Toe/src/Main.java
@@ -1,25 +1,46 @@
+import java.util.Scanner;
+
 import GameLogic.Game;
 import Grid.Grid;
 
 public class Main {
+
+    private static Scanner sc = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        Grid grid = new Grid();
-        Game game = new Game(grid);
-
-        game.displayStats();
-        grid.displayGrid();
-
         //main game loop
-        while (!game.isGameOver()) {
-            // handle player 1's turn
-            game.getPlayer1().handleTurn();
+        while (true) {
+            Grid grid = new Grid();
+            Game game = new Game(grid);
 
-            if (game.isGameOver()) { break; }
+            game.displayStats();
+            grid.displayGrid();
 
-            // handle player 2's turn
-            game.getPlayer2().handleTurn();
+            // alternate turns while game is in progress
+            while (!game.isGameOver()) {
+                // handle player 1's turn
+                game.getPlayer1().handleTurn();
+
+                if (game.isGameOver()) { break; }
+
+                // handle player 2's turn
+                game.getPlayer2().handleTurn();
+            }
+            
+            game.displayFinalGameState();
+
+            String input = replay();
+            if (!input.equals("1")) {
+                break;
+            }
         }
-        
-        game.displayFinalGameState();
+    }
+
+    private static String replay() {
+        System.out.println("Do you want to replay? ");
+        System.out.println("To replay press 1 to exit press any other key: ");
+
+        String input = sc.nextLine();
+        return input;
     }
 }
